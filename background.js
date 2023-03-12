a3cb6d43f6239cf059d8537882112152e941b84c
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2023-03-12 12:57:15 +0530

    Redirection to Google.com

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       if (details.url.indexOf("instagram.com") != -1 || details.url.indexOf("web.whatsapp.com") != -1) {
-        return {redirectUrl: "https://www.youtube.com"};
+        return {redirectUrl: "https://www.google.com"};
       }
     },
     {urls: ["*://*.instagram.com/*", "*://web.whatsapp.com/*"]},
