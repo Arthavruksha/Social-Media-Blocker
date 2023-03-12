b6a176deed66daeb6ad2c1302df45b5499a4d123
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2023-03-12 12:56:30 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      if (details.url.indexOf("instagram.com") != -1 || details.url.indexOf("web.whatsapp.com") != -1) {
+        return {redirectUrl: "https://www.youtube.com"};
+      }
+    },
+    {urls: ["*://*.instagram.com/*", "*://web.whatsapp.com/*"]},
+    ["blocking"]
+  );
+  
