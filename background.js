7605e3c9f6843eff516c6d2aa1ae4fba6e6dfe69
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2023-06-06 13:33:05 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-      if (details.url.indexOf("instagram.com") != -1 || details.url.indexOf("web.whatsapp.com") != -1) {
+      if (details.url.indexOf("instagram.com") != -1 || details.url.indexOf("web.whatsapp.com") != -1 || details.url.indexOf("twitter.com") != -1 ) {
         return {redirectUrl: "https://www.google.com"};
       }
     },
